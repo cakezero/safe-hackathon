d8fb5900b7afdff8dc79fc2c95961e4c55ca8f4a
cakezero <sareahj.com@gmail.com>
2025-02-10 04:26:06 +0100

    mod: token edits, erc20 and token abi with bytecode

--- frontend/src/token/token.ts
+++ frontend/src/token/token.ts
@@ -1,7 +1,9 @@
-import { SupportedChainId, SigningScheme, OrderBookApi, OrderQuoteRequest, OrderQuoteSideKindSell, OrderSigningUtils, UnsignedOrder } from "@cowprotocol/cow-sdk";
+import { SupportedChainId, SigningScheme, OrderBookApi, OrderSigningUtils, UnsignedOrder } from "@cowprotocol/cow-sdk";
 import { ethers, Signer } from "ethers"
 import { Web3Provider } from "@ethersproject/providers";
-import { abi, bytecode } from "./erc/erc20"
+import { abi } from "./erc/erc20"
+import { tokenProp } from "./erc/tokenProp"
+import { getETHQuote, getTokenQuote } from "./quote";
 
 const relayerAddress = "0x"
 const WETH = "0xfff9976782d46cc05630d1f6ebab18b2324d6b14";
@@ -26,16 +28,7 @@ const TokenSwap = async (provider: Web3Provider, tokenAddress: string, tokenAmou
 
     const orderBook = new OrderBookApi({ chainId: SupportedChainId.SEPOLIA });
 
-    const quoteRequest: OrderQuoteRequest = {
-      sellToken: tokenAddress,
-      buyToken: WETH,
-      from: userAddress,
-      receiver: userAddress,
-      sellAmountBeforeFee: tokenAmount,
-      kind: OrderQuoteSideKindSell.SELL,
-    };
-
-    const { quote } = await orderBook.getQuote(quoteRequest);
+    const quote = await getTokenQuote(orderBook, tokenAddress, userAddress, tokenAmount, WETH)
 
     const order: UnsignedOrder = {
       ...quote,
@@ -85,18 +78,9 @@ const ETHSwap = async (provider: Web3Provider, tokenAddress: `0x${string}`, ethA
     console.log("receipt:", receipt, "\n");
   
     const orderBook = new OrderBookApi({ chainId: SupportedChainId.SEPOLIA });
-  
-    const quoteRequest: OrderQuoteRequest = {
-      sellToken: WETH,
-      buyToken: tokenAddress,
-      from: userAddress,
-      receiver: userAddress,
-      sellAmountBeforeFee: ethAmount,
-      kind: OrderQuoteSideKindSell.SELL,
-    };
-  
-    const { quote } = await orderBook.getQuote(quoteRequest);
-  
+
+    const quote = await getETHQuote(orderBook, tokenAddress, userAddress, ethAmount, WETH);
+
     const order: UnsignedOrder = {
       ...quote,
       sellAmount: ethAmount,
@@ -133,25 +117,27 @@ const ETHSwap = async (provider: Web3Provider, tokenAddress: `0x${string}`, ethA
 const deployToken = async (provider: Web3Provider, tokenName: string, tokenQuantity: number, tokenSymbol: string) => {
   const signer = provider.getSigner();
 
-  const token = new ethers.ContractFactory(abi, bytecode, signer as unknown as Signer);
+  const token = new ethers.ContractFactory(tokenProp.abi, tokenProp.bytecode, signer as unknown as Signer);
 
   const deployedToken = await token.deploy(tokenName, tokenSymbol, tokenQuantity);
 
   const tokenHash = deployedToken.deploymentTransaction()?.hash;
 
   await deployedToken.waitForDeployment();
-
-  const tokenAddress = await deployedToken.getTokenAddress();
-
+  
   const deployedTokenAddress = await deployedToken.getAddress();
+
+  const tokenContract = new ethers.Contract(deployedTokenAddress, tokenProp.abi, signer as unknown as Signer)
+
+  const tokenAddress = await tokenContract.getTokenAddress();
 
   return { deployedTokenAddress, tokenHash, tokenAddress };
 }
 
-const addLiquidity = async (provider: Web3Provider, tokenAddress: string, ethAmount: string, tokenAmount: number) => { 
+const addLiquidity = async (provider: Web3Provider, tokenContractAddress: string, ethAmount: string, tokenAmount: number) => { 
   const signer = provider.getSigner();
 
-  const tokenContract = new ethers.Contract(tokenAddress, abi, signer as unknown as Signer)
+  const tokenContract = new ethers.Contract(tokenContractAddress, tokenProp.abi, signer as unknown as Signer)
 
   const liquidityAdd = await tokenContract.AddLiquidityETH(tokenAmount, { value: ethers.parseUnits(ethAmount, "ether") });
 
