21c12db8de2d4c4a0b0c5e1daffb10fcc09cd464
cakezero <sareahj.com@gmail.com>
2025-02-10 12:25:59 +0100

    feat: save token api

--- server/server.ts
+++ server/server.ts
@@ -31,7 +31,39 @@ const tokenSchema = new mongoose.Schema({
 
 const token = mongoose.model("tokesProp", tokenSchema)
 
+server.use("/save-token", async (req: Request, res: Response) => {
+  try {
+    const { owner, fileId, tokenAddress, tokenFactory } = req.body;
+    const tokenExists = await token.findOne({ owner });
 
+    if (!tokenExists) {
+      const fileArr = [fileId];
+      const tokenArr = [tokenAddress];
+      const factoryArr = [tokenFactory];
+      const createdToken = new token({
+        owner,
+        fileId: fileArr,
+        tokenAddress: tokenArr,
+        tokenFactory: factoryArr,
+      });
+      createdToken.save();
+      res
+        .status(201)
+        .json({ message: "Token information saved", createdToken });
+      return;
+    }
+
+    tokenExists.fileId.push(fileId);
+    tokenExists.tokenAddress.push(tokenAddress);
+    tokenExists.tokenFactory.push(tokenFactory);
+    tokenExists.save();
+
+    res.status(200).json({ message: "Token saved successfully", tokenExists });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 server.use("/fetch-token", async (req: Request, res: Response) => {
   try {
