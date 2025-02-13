382a489d045f4f2d1cd86ad1f0240a7c5bd20350
cakezero <sareahj.com@gmail.com>
2025-02-13 10:12:35 +0100

    toaster for user feedback

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "../pages/Home";
 import Tokens from "../pages/Tokens";
 import NavBar from "../components/navbar/NavBar";
+import { Toaster } from "react-hot-toast";
 
 export default function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
                 <Route path="/" element={<Home />} />
                 <Route path="/tokens" element={<Tokens />} />
             </Routes>
+            <Toaster />
         </BrowserRouter>
     )
 }
