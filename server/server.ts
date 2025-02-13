6cbf9dd84676669ab385cf0207fb03a072c18dae
cakezero <sareahj.com@gmail.com>
2025-02-13 10:10:49 +0100

    server edit

--- server/server.ts
+++ server/server.ts
@@ -23,59 +23,78 @@ const tokenSchema = new mongoose.Schema({
   tokenFactory: [{
     type: String
   }],
+  tokenName: [{
+    type: String
+  }],
+  tokenBalance: [{
+    type: Number
+  }],
   owner: {
     type: String,
-    required: true
-  }
+    required: true,
+    unique: true
+  },
+  tokenSymbol: [{
+    type: String
+  }]
 });
 
 const token = mongoose.model("tokesProp", tokenSchema)
 
-server.use("/save-token", async (req: Request, res: Response) => {
+server.post("/save-token", async (req: Request, res: Response) => {
   try {
-    const { owner, fileId, tokenAddress, tokenFactory } = req.body;
+    const { owner, fileId, tokenAddress, tokenFactory, tokenSymbol, tokenName, tokenBalance } = req.body;
     const tokenExists = await token.findOne({ owner });
 
     if (!tokenExists) {
       const fileArr = [fileId];
       const tokenArr = [tokenAddress];
       const factoryArr = [tokenFactory];
-      const createdToken = new token({
+      const balanceArr = [tokenBalance];
+      const nameArr = [tokenName];
+      const symbolArr = [tokenSymbol]
+      const tokenProp = new token({
         owner,
         fileId: fileArr,
         tokenAddress: tokenArr,
         tokenFactory: factoryArr,
+        tokenBalance: balanceArr,
+        tokenName: nameArr,
+        tokenSymbol: symbolArr
       });
-      createdToken.save();
+      await tokenProp.save();
       res
         .status(201)
-        .json({ message: "Token information saved", createdToken });
+        .json({ message: "Token information saved", tokenProp });
       return;
     }
 
-    tokenExists.fileId.push(fileId);
-    tokenExists.tokenAddress.push(tokenAddress);
-    tokenExists.tokenFactory.push(tokenFactory);
+    if (!tokenExists.fileId.includes(fileId)) tokenExists.fileId.push(fileId);
+    if (!tokenExists.tokenAddress.includes(tokenAddress)) tokenExists.tokenAddress.push(tokenAddress);
+    if (!tokenExists.tokenName.includes(tokenName)) tokenExists.tokenName.push(tokenName);
+    if (!tokenExists.tokenFactory.includes(tokenFactory)) tokenExists.tokenFactory.push(tokenFactory);
+    if (!tokenExists.tokenBalance.includes(tokenBalance)) tokenExists.tokenBalance.push(tokenBalance);
+    if (!tokenExists.tokenSymbol.includes(tokenSymbol)) tokenExists.tokenSymbol.push(tokenSymbol);
     tokenExists.save();
 
-    res.status(200).json({ message: "Token saved successfully", tokenExists });
+    res.status(200).json({ message: "Token saved successfully", tokenProp: tokenExists });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
 
-server.use("/fetch-token", async (req: Request, res: Response) => {
+server.get("/fetch-token", async (req: Request, res: Response) => {
   try {
-    const { owner } = req.body;
-    const tokenOwner = await token.find({ owner });
+    const { owner } = req.query;
+    const tokenFound = await token.find({ owner });
 
-    if (!tokenOwner) {
+    if (!tokenFound) {
       res.status(400).json({ error: "No user found" })
       return;
     }
 
-    res.status(200).json({ message: "Token information fetched", tokenOwner })
+    res.status(200).json({ message: "Token information fetched", tokenFound })
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
